f809e6c08c447318f0394d02aeea3278a05d3d89
RatnaRajpal <54902237+RatnaRajpal@users.noreply.github.com>
2020-06-02 20:02:50 +0530

    Update Odd_Even.java

--- Odd_Even.java
+++ Odd_Even.java
@@ -1,7 +1,8 @@
+import java.util.Scanner;
 public class Odd_Even {
 public static void main(String args[]) {
 	System.out.print("Enter the number :");
-	int num=new Scanner(System.in).nextInt();// Anonymous object of scanner class
+	int num=new Scanner(System.in).nextInt();
 	System.out.println((num%2 == 0)? ("The number "+num+" is a Even number"):("The number "+num+" is a Odd number"));
 }
 }
