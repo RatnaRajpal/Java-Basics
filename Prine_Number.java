d726a21a2a7c3d719ef022ad6f2093eb49c9a1a8
RatnaRajpal <54902237+RatnaRajpal@users.noreply.github.com>
2020-06-02 20:11:39 +0530

    Create Prine_Number.java

---
+++ Prine_Number.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Prime_Number {
+	void check(int num) {
+		int i;
+		for(i=2;i<num;i++) {
+			if(num%i ==0) {
+				break;
+			}
+		}
+		if(i==num) {
+			System.out.println("The number "+num+" is a prime number.");
+		}
+		else
+			System.out.println("The number "+num+" is not a prime number.");
+	
+}
+	public static void main(String args[]) {
+		System.out.println("Enter the number :");
+		int num=new Scanner(System.in).nextInt();
+		Prime_Number prime=new Prime_Number();
+		
+		prime.check(num);
+}
+}
