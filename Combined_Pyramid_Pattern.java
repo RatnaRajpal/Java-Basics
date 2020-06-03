dbf7535b6a7c199534b0b6f400241b6cbe780309
RatnaRajpal <54902237+RatnaRajpal@users.noreply.github.com>
2020-06-03 20:16:49 +0530

    Create Combined_Pyramid_Pattern.java

---
+++ Combined_Pyramid_Pattern.java
@@ -0,0 +1,40 @@
+/* Implements the following pattern
+ *
+ **
+ ***
+ ****
+ *****
+ ******
+ *****
+ ****
+ ***
+ **
+ *
+ */
+import java.util.Scanner;
+
+public class Combined_Pyramid_Pattern {
+	void pyramid(int num) {
+		int k=2;
+		for(int i=0;i<2*num-1;i++) {
+			for(int j=0;j<i;j++) {
+				if((j<=i &&i<num)||(i>=num&&j<num-k))
+				System.out.print("*");
+				else {
+			      k++;
+			      break;
+			}
+			}
+			System.out.println();
+		
+	}
+	}
+	public static void main(String args[]) {
+	
+		System.out.println("Enter the number of rows:");
+		Scanner sc=new Scanner(System.in);
+		int num =sc.nextInt();
+		Combined_Pyramid_Pattern   py=new Combined_Pyramid_Pattern ();
+		py.pyramid(num);
+	}
+}
